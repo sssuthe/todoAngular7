8179d958b7ecd5702716af0c9c47289618f7dc83
Sean Sutherland <sssuthe@gmail.com>
2019-08-05 23:11:39 -0600

    adding https to the equation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   resourceServer: {
-    todoUrl: 'http://sssuthe.com/api/todo/',
-    generateUidUrl: 'http://sssuthe.com/api/generate_uid/',
-    externalPingUrl: 'http://sssuthe.com/api/external/'
+    todoUrl: 'https://sssuthe.com/api/todo/',
+    generateUidUrl: 'https://sssuthe.com/api/generate_uid/',
+    externalPingUrl: 'https://sssuthe.com/api/external/'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   resourceServer: {
-    todoUrl: 'http://localhost/api/todo/',
-    generateUidUrl: 'http://localhost/api/generate_uid/',
-    externalPingUrl: 'http://localhost/api/external/'
+    todoUrl: 'https://localhost/api/todo/',
+    generateUidUrl: 'https://localhost/api/generate_uid/',
+    externalPingUrl: 'https://localhost/api/external/'
   }
 };
