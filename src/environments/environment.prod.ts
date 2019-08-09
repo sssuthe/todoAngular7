42675e6ed9894ef9bb176a6872029bc4a45fdbfd
Sean Sutherland <sssuthe@gmail.com>
2019-08-08 21:00:39 -0600

    updating api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   resourceServer: {
-    todoUrl: 'https://sssuthe.com/api/todo/',
-    generateUidUrl: 'https://sssuthe.com/api/generate_uid/',
-    externalPingUrl: 'https://sssuthe.com/api/external/'
+    todoUrl: 'https://sssuthe.com/api/v1/todo/',
+    generateUidUrl: 'https://sssuthe.com/api/v1/generate_uid/',
+    externalPingUrl: 'https://sssuthe.com/api/v1/external/'
   }
 };
